314cd4b4d2e19634c07c4f393a592c33211bc333
Emil Wiards <emil@wiards.net>
2024-12-02 08:48:47 +0100

    Use `for/*` functions instead of `disjoin` and `andmap`

--- 2024/day02/puzzle1.rkt
+++ 2024/day02/puzzle1.rkt
@@ -1,17 +1,17 @@
 #lang racket
 
-;; function that simplify input handling
+;; functions that simplify input handling
 (require 2htdp/batch-io
          srfi/1)
 
 (define (safe? report)
   (let ([diff-safe? (λ (x y) (let ([diff (abs (- x y))])
                                (and (1 . <= . diff)
-                                    (diff . <= . 3))))]
-        [len (- (length report) 1)])
-    ((disjoin (curry andmap (conjoin diff-safe? <=))
-              (curry andmap (conjoin diff-safe? >=)))
-     (take report len) (drop report 1))))
+                                    (diff . <= . 3))))])
+    (for/or ([lt (list <= >=)])
+      (for/and ([x report]
+                [y (rest report)])
+        (and (diff-safe? x y) (lt x y))))))
 
 (define (solve path)
   (let*-values ([(reports) (read-words-and-numbers/line path)])
