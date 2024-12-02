e9196be933172b4603f76233af25ad0096ff450c
Emil Wiards <emil@wiards.net>
2024-12-02 08:50:23 +0100

    Add solution to day 2 puzzle 2

---
+++ 2024/day02/puzzle2.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+;; functions that simplify input handling
+(require 2htdp/batch-io
+         srfi/1)
+
+(define (safe? report)
+  (let ([diff-safe? (λ (x y) (let ([diff (abs (- x y))])
+                               (and (1 . <= . diff)
+                                    (diff . <= . 3))))])
+    (for*/or ([comb (in-combinations report (sub1 (length report)))]
+              [lt (list <= >=)])
+      (for/and ([x comb]
+                [y (rest comb)])
+        (and (diff-safe? x y)
+             (lt x y))))))
+
+(define (solve path)
+  (let*-values ([(reports) (read-words-and-numbers/line path)])
+    (count safe? reports)))
+
+(solve "input.txt")
