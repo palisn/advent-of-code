5fddcdbc989e6a804f72e7fde7209443a6a12d7b
Emil Wiards <emil@wiards.net>
2024-12-03 18:02:17 +0100

    Convert unnecessary `let*` to `let`

--- 2024/day03/puzzle2.rkt
+++ 2024/day03/puzzle2.rkt
@@ -1,8 +1,8 @@
 #lang racket
 
 (define (sum-mul-instructions str)
-  (let* ([context-pattern #px"(^|do\\(\\)).*?(don't\\(\\)|$)"]
-         [pattern #px"mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"])
+  (let ([context-pattern #px"(^|do\\(\\)).*?(don't\\(\\)|$)"]
+        [pattern #px"mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"])
     (for*/sum ([sstr (regexp-match* context-pattern str)]
                [factors (regexp-match* pattern sstr #:match-select rest)])
       (apply * (map string->number factors)))))
