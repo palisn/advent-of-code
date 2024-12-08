f1046798331b4103e48e76ddddc8f7f7819af7cd
Emil Wiards <emil@wiards.net>
2024-12-08 12:00:47 +0100

    Add day 8

---
+++ 2024/day08/puzzle2.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(define (antinode-count arr)
+  (define-values (width height) (values (vector-length (vector-ref arr 0))
+                                        (vector-length arr)))
+  
+  (define antennas-by-frequency
+    (for*/fold ([freqs (hash)])
+               ([x (in-range width)]
+                [y (in-range height)])
+      (let ([freq (vector-ref (vector-ref arr y) x)])
+        (cond
+          [(char=? freq #\.) freqs]
+          [(hash-has-key? freqs freq)
+           (hash-set freqs freq (set-add (hash-ref freqs freq)
+                                         (list x y)))]
+          [else (hash-set freqs freq (set (list x y)))]))))
+  
+  (define/match (in-bounds p)
+    [(`(,x ,y)) (and (and (0 . <= . x) (x . < . width))
+                     (and (0 . <= . y) (y . < . height)))])
+  
+  (define (antinodes p q)
+    (let* ([diff (map - p q)]
+           [anode (λ (n) (map + p (map (curry * n) diff)))])
+      (append (for/list ([n (in-naturals 0)])
+                #:break (not (in-bounds (anode n)))
+                (anode n))
+              (for/list ([n (in-naturals 1)])
+                #:break (not (in-bounds (anode (- n))))
+                (anode (- n))))))
+
+  (set-count
+   (for*/set ([(freq locs) antennas-by-frequency]
+              [l1 locs]
+              [l2 locs]
+              #:unless (equal? l1 l2)
+              [antinode (antinodes l1 l2)])
+     antinode)))
+
+
+(define (solve path)
+  (let*-values ([(arr) (list->vector
+                        (map (compose list->vector string->list)
+                             (file->lines path)))])
+    (antinode-count arr)))
+
+(solve "input.txt")
